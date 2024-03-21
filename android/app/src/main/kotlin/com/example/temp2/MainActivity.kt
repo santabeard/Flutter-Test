77deec29e659691f5be1f1a16bc885f7b1878f19
Miro Ali Akbar <Miroaliakbar@gmail.com>
2024-03-21 15:13:58 +0100

    inital

---
+++ android/app/src/main/kotlin/com/example/temp2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.temp2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cloud_firestore
+import firebase_core
+import path_provider_foundation
+import upgrade
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UpgradePlugin.register(with: registry.registrar(forPlugin: "UpgradePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
